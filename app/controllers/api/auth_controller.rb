d0d3bc3dd8227a138683f714830221252428c0cd
Josh Tate <joshtate04@me.com>
2018-02-26 18:56:02 -0500

    Authentication fix

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,24 +1,36 @@
 module Api
   class AuthController < ApiController
+    before_action :authenticate_user, only: :create
     skip_before_action :authenticate_request
 
     def create
-      command = AuthenticateUser.call(params[:email], params[:password])
-
-      if command.success?
+      if @user
         render json: {
           authentication: {
-            authToken: command.result,
-            user: command.user
+            authToken: auth_token,
+            user: @user
           }
         }
       else
-        render json: {error: command.errors}, status: :unauthorized
+        render json: {error: 'Invalid Username or Password'}, status: :unauthorized
       end
     end
 
     def me
       render json: {current_user: current_user}
+    end
+
+  private
+
+    def authenticate_user
+      @user = User.find_by(email: params[:email])
+      return @user if @user && @user.authenticate(params[:password])
+      
+      nil
+    end
+
+    def auth_token
+      JsonWebToken.encode(user_id: @user.id) if @user
     end
   end
 end
